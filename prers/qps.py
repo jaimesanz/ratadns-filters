8123433631bc8db4dce27a74b2793761c68ce6db
raticate <caterina.munoz.v@gmail.com>
2015-08-27 16:08:55 -0300

    aps y qps

---
+++ prers/qps.py
@@ -0,0 +1,26 @@
+__author__ = 'raticate'
+
+import time
+
+class QueriesPerSecond(object):
+    def __init__(self, f):
+        self.counter = 0
+        self.start = time.time()
+        self.f = f
+
+    def __call__(self, d):
+        flags =  int(d['flags'], 16)
+        is_answer = (flags & ( 1 << 15 )) == (1 << 15)
+        if not is_answer:
+            self.counter += 1
+
+    def get_data(self):
+        data = { 'pps' : self.counter / (time.time() - self.start) }
+        return data
+
+    def get_file(self):
+        return self.f
+
+    def reset(self):
+        self.counter = 0
+        self.start = time.time()
