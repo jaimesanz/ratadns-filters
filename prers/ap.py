0d4fd82c0a6211f565e3b2188d57652e03c52fc6
Manuel Cáceres Reyes <el.ariel.cl@gmail.com>
2015-09-03 16:16:38 -0300

    Remove unnecesary fields and Separate alone packets into queries and answers

--- prers/ap.py
+++ prers/ap.py
@@ -5,51 +5,32 @@ __author__ = 'sking32'
 # answers without queries)
 class AlonePackets(object) :
     def __init__(self, f):
-        self.pcounter = 0
         self.qcounter = 0
         self.acounter = 0
-        self.alpcounter = 0
-        self.alqcounter = 0
-        self.alacounter = 0
         self.f = f
         self.aloneQueriesIds = {} #Dicts {id : InputDict}
         self.aloneAnswersIds = {}
-        self.alonePacketsIds = {}
 
     def __call__(self, d):
         flags =  int(d['flags'], 16)
         id = d['id']
         is_answer = (flags & ( 1 << 15 )) == (1 << 15)
-        self.pcounter += 1
         if is_answer:
             self.acounter += 1
             if self.aloneQueriesIds.has_key(id) :
-                self.alonePacketsIds.pop(id, None)
-                self.alpcounter -= 1
                 self.aloneQueriesIds.pop(id, None)
-                self.alqcounter -= 1
             else :
-                self.alonePacketsIds[id] = d
-                self.alpcounter += 1
                 self.aloneAnswersIds[id] = d
-                self.alacounter += 1
 
         else:
             self.qcounter += 1
             if self.aloneAnswersIds.has_key(id) :
-                self.alonePacketsIds.pop(id, None)
-                self.alpcounter -= 1
                 self.aloneAnswersIds.pop(id, None)
-                self.alacounter -= 1
-
             else :
-                self.alonePacketsIds[id] = d
-                self.alpcounter += 1
                 self.aloneQueriesIds[id] = d
-                self.alqcounter += 1
-
+        
     def get_data(self):
-        data = {'queries' : self.qcounter, 'answers' : self.acounter, 'AlonePackets' : self.alonePacketsIds.values()}
+        data = {'queries' : self.qcounter, 'answers' : self.acounter, 'AloneAnswers' : self.aloneAnswersIds.values(), 'AloneQueries' : self.aloneQueriesIds.values()}
         return data
 
     def get_file(self):
@@ -59,5 +40,4 @@ class AlonePackets(object) :
         self.pcounter = self.qcounter = self.acounter = self.alpcounter = self.alqcounter = self.alacounter = 0
         self.aloneQueriesIds = {} #Dicts {id : InputDict}
         self.aloneAnswersIds = {}
-        self.alonePacketsIds = {}
 
