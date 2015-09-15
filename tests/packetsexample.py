8d7419338f365e963df65b340e01af9b60ad13c3
Manuel Cáceres Reyes <el.ariel.cl@gmail.com>
2015-09-15 18:03:54 -0300

    Add functions for storing information

--- tests/packetsexample.py
+++ tests/packetsexample.py
@@ -5,9 +5,10 @@ from random import shuffle
 #encapsulates packets given to prs
 #with their expected results
 class PacketsExample:
-    def __init__(self, expected = {}):
+    def __init__(self, expected = {}, information = {}):
         self.__data = []
         self.__expected = expected
+        self.__information = information
 
     def __iter__(self):
         shuffle(self.__data)
@@ -18,6 +19,12 @@ class PacketsExample:
 
     def setExpected(self, key, value):
         self.__expected[key] = value
+
+    def putInformation(self, key, value):
+        self.__information[key] = value
+
+    def getInformation(self, key):
+        return self.__information[key]
 
     def expectedValue(self, key):
         if not self.__expected.has_key(key) :
