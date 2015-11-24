6d77aa2048d2f09dfb444269a9a1062fb7906aef
tomas <matias.cisterna.m@outlook.com>
2015-11-24 14:42:58 -0300

    tests for traffic_volume_queries and responses

--- prers/__init__.py
+++ prers/__init__.py
@@ -29,5 +29,7 @@ from qtype import Qtype
 from edns_version import EdnsVersion
 from traffic_sizes_queries import TrafficSizesQueries
 from traffic_sizes_responses import TrafficSizesResponses
+from traffic_volume_queries import TrafficVolumeQueries
+from traffic_volume_responses import TrafficVolumeResponses
 
 __author__ = 'Francisco Cifuentes'
